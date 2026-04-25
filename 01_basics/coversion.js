783f3af39838e71595954497a12af8c73a9b809b
ishujordan <145466993+ishujordan@users.noreply.github.com>
2026-04-25 07:28:46 +0000

    update

---
+++ 01_basics/coversion.js
@@ -0,0 +1,48 @@
+let score = "33abc" ///not a number =. NaN/// 
+
+console.log (typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+
+let no = null 
+
+console.log(typeof no )
+
+console.log(no)
+
+
+let yes =  true 
+ console.log(yes)
+
+
+ /// true = 1 , false  =   0 //
+
+
+ let isLoggedIn = 1
+
+ let booleanisLoggedIn = Boolean(isLoggedIn);
+
+ console.log(booleanisLoggedIn)
+
+
+ 
+let  dataset = ""
+
+let testondataset = Boolean(dataset)
+
+
+console.log(testondataset)
+
+
+ ///EMPTY STRING IS ALSO EQUAL TO FALSE IN BOOLEAN AND A FILLED STRING WILL HAVE THE OPPOSITE EFFECT  //
+
+
+ 
+
+
+
+ 
+ 
