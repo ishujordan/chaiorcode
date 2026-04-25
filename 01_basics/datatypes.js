c8ea4709af0d7d5694cbd44bc6a200ab1f6edb25
ishujordan <145466993+ishujordan@users.noreply.github.com>
2026-04-25 06:47:56 +0000

    configuration

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 +3 ) //we ng node using node js not browser////
+
+console.log (3 +3); console.log("hitesh")/// readebiltiy is neccesary ///
+
+
+let name = "mohit"
+
+let age  = 18
+ 
+let  LoggedIn = false 
+
+//number => 2 to the power 53 
+
+//bigInt 
+
+//string =>""
+
+//boolean => true/false
+
+//null  => standalone value / mean its just empty / zero kinf of 
+
+//undefined => when nothing has been defined 
+
+//symbol =>  unique 
+
+//object
+
+
+console.log(typeof null )///type object 
+
+console.log(typeof undefined ) //type undefined
+
+
+
